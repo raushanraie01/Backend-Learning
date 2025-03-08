1e5c5b8c036276b748fb076ffd59abe9f8dd13f8
Raushan Kumar <raushanraie01@gmail.com>
2025-03-09 01:45:36 +0530

    Config File Done

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Done");
+});
+console.log(process.env.PORT);
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is Running on port ${process.env.PORT}`);
+});
